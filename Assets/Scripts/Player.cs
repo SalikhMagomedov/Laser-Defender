6cd06bc38769a19a1a0e78b7422fd498a9a3810c
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-26 20:59:22 +0300

    Movement & Time.deltaTime

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+
+    // Update is called once per frame
+    void Update() => Move();
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = transform.position.x + deltaX;
+        var newYPos = transform.position.y + deltaY;
+        
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
