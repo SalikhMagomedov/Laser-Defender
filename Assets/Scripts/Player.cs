000103edb7011cc7c15b384038e24982e7571b9b
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-26 21:35:57 +0300

    ViewPortToWorldPoint()

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,25 @@
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float padding = 1f;
+    
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+    private void Start() => SetUpMoveBoundaries();
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
 
     // Update is called once per frame
     void Update() => Move();
@@ -12,8 +31,8 @@ public class Player : MonoBehaviour
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         
         transform.position = new Vector2(newXPos, newYPos);
     }
