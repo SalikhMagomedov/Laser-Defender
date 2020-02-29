0d9da59de0aa5a50a87fb9939ff71aedd38e86b2
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-29 12:47:43 +0300

    Spawn Multiple Enemy Waves

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,12 +6,9 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<WaveConfig> waveConfigs;
 
-    private int startigWave = 0;
-
     private void Start()
     {
-        var currentWave = waveConfigs[startigWave];
-        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        StartCoroutine(SpawnAllWaves());
     }
 
     private IEnumerator SpawnAllEnemiesInWave(WaveConfig wave)
@@ -21,6 +18,14 @@ public class EnemySpawner : MonoBehaviour
             var enemy = Instantiate(wave.EnemyPrefab, wave.Waypoints[0].position, Quaternion.identity);
             enemy.GetComponent<EnemyPathing>().WaveConfig = wave;
             yield return new WaitForSeconds(wave.TimeBetweenSpawns);
+        }
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        foreach (var wave in waveConfigs)
+        {
+            yield return SpawnAllEnemiesInWave(wave);
         }
     }
 }
