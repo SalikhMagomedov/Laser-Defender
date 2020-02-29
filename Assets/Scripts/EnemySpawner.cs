519e7b394d1f20e78ebaf0ebcf3cf7a814d3908d
Salikh Magomedov <sm.saarus@gmail.com>
2020-02-29 11:37:10 +0300

    Spawn Multiple Enemies

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private List<WaveConfig> waveConfigs;
+
+    private int startigWave = 0;
+
+    private void Start()
+    {
+        var currentWave = waveConfigs[startigWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig wave)
+    {
+        for (int i = 0; i < wave.NumberOfEnemies; i++)
+        {
+            Instantiate(wave.EnemyPrefab, wave.Waypoints[0].position, Quaternion.identity);
+            yield return new WaitForSeconds(wave.TimeBetweenSpawns);
+        }
+    }
+}
