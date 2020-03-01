f8df43b17422ae02bd63bc5c51d69c47e0b7dfb5
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-01 21:20:58 +0300

    Create Damage Dealer Class

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 100;
+
+    public int Damage => damage;
+
+    public void Hit() => Destroy(gameObject);
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float health = 100;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
+
+        health -= damageDealer.Damage;
+        damageDealer.Hit();
+    }
+}
