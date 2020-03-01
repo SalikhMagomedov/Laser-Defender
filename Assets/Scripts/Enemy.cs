8eb2362a4f8f357b26ef13c30339c3f9416109e3
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-01 21:28:17 +0300

    Destroy Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,8 +7,16 @@ public class Enemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
 
+    private void ProcessHit(DamageDealer damageDealer)
+    {
         health -= damageDealer.Damage;
         damageDealer.Hit();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
