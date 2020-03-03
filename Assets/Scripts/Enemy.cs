7df8f4355b54196ef8327f347e3206cec327441e
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-03 21:01:17 +0300

    Make Enemy Shoot

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,38 @@
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float health = 100;
+    [SerializeField] private float minTimeBetweenShots = 0.2f;
+    [SerializeField] private float maxTimeBetweenShots = 3f;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private float projectileSpeed = 10f;
+
+    private float shotCounter;
+
+    private void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    private void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+        var laser = Instantiate(projectile, transform.position, Quaternion.identity);
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
