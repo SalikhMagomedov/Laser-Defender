f4f2cc2e9a4d22d7d0be4a9f66cc098d6364d1dd
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-04 22:20:20 +0300

    Load All The Scenes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void LoadStartMenu() => SceneManager.LoadScene(0);
+
+    public void LoadGame() => SceneManager.LoadScene("Game");
+
+    public void LoadGameOver() => SceneManager.LoadScene("Game Over");
+
+    public void QuitGame() => Application.Quit();
+}
