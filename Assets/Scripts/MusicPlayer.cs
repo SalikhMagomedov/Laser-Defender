abd64552ea236414e763be5b929ad56956b08341
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-05 20:42:49 +0300

    Music Player With Singleton

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake() => SetUpSingleton();
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
